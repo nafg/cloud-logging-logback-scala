5e9c0a48cad550a9420e08adbc53ff95b52307bc
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2023-08-14 23:43:57 -0400

    JsonMarker#apply to use an Encoder on a scala object

--- marker/src/main/scala/io/github/nafg/cloudlogging/marker/JsonMarker.scala
+++ marker/src/main/scala/io/github/nafg/cloudlogging/marker/JsonMarker.scala
@@ -1,20 +1,12 @@
 package io.github.nafg.cloudlogging.marker
 
-import scala.jdk.CollectionConverters._
-
-import io.circe.Json
+import io.circe.{Encoder, Json}
 import org.slf4j.Marker
 
-
-case class JsonMarker(name: String, data: Json, references: Marker*) extends Marker {
-  override def getName = name
-  override def add(reference: Marker): Unit = throw new NotImplementedError
-  override def remove(reference: Marker) = throw new NotImplementedError
-  override def hasChildren = hasReferences
-  override def hasReferences = references.nonEmpty
-  override def iterator() = references.asJava.iterator()
-  override def contains(other: Marker) =
-    this == other || references.exists(_.contains(other))
-  override def contains(name: String) =
-    this.name == name || references.exists(_.contains(name))
+case class JsonMarker(name: String, data: Json, references: Marker*)
+    extends BasicMarker(name, references)
+object JsonMarker {
+  def apply[A: Encoder](name: String,
+                        references: Marker*)(data: A): JsonMarker =
+    JsonMarker(name, Encoder[A].apply(data), references: _*)
 }
